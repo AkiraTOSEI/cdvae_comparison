9e4d2f2b77d2e42095de78ea2ff45f3c871e71a7
Akihiro Fujii <akihiro.fujii0137@gmail.com>
2025-04-16 21:42:27 +0900

    megnet の学習率・ステップの最適化完了

--- megnet_optimization_hpo.sh
+++ megnet_optimization_hpo.sh
@@ -2,12 +2,15 @@
 
 # 学習率とステップ数の候補リスト
 lrs=(0.00001 0.0001 0.001 0.01)
-steps=(200 400 800 1600 3200 5000)
+steps=(400 800 1600 3200 5000 200)
 
-# 共通設定
+# 実行コマンド
+# bash megnet_optimization_hpo.sh
 
 # 2重ループで条件を全探索
 for lr in "${lrs[@]}"; do
   for step in "${steps[@]}"; do
     echo "▶ Running with lr=$lr, num_gradient_steps=$step"
-    poetry run python scripts/evaluate.py --tasks opt --label megnet --lr "$lr" --num_starting_points 128 --model_path /home/fujii/cdvae_comparison/hydra/singlerun/2025-04-15/shin_megnet_lr5e-6/ --target_bg 2.5 --num_saved_crys 0 --megnet_loss_mode True --coef_e_form 0 --num_gradient_steps "$steps"
+    poetry run python scripts/evaluate.py --tasks opt --label megnet --lr $lr --num_starting_points 128 --model_path /home/fujii/cdvae_comparison/hydra/singlerun/2025-04-15/shin_megnet_lr5e-6/ --target_bg 2.5 --num_saved_crys 0 --megnet_loss_mode True --coef_e_form 0 --num_gradient_steps $step
+  done
+done
