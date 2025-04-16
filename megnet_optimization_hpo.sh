13d857c3ffbf01887057756fcb518685f300f643
Akihiro Fujii <akihiro.fujii0137@gmail.com>
2025-04-16 19:03:48 +0900

    SC最適化で変な値が出たが、いったん保存...

---
+++ megnet_optimization_hpo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 学習率とステップ数の候補リスト
+lrs=(0.00001 0.0001 0.001 0.01)
+steps=(200 400 800 1600 3200 5000)
+
+# 共通設定
+
+# 2重ループで条件を全探索
+for lr in "${lrs[@]}"; do
+  for step in "${steps[@]}"; do
+    echo "▶ Running with lr=$lr, num_gradient_steps=$step"
+    poetry run python scripts/evaluate.py --tasks opt --label megnet --lr "$lr" --num_starting_points 128 --model_path /home/fujii/cdvae_comparison/hydra/singlerun/2025-04-15/shin_megnet_lr5e-6/ --target_bg 2.5 --num_saved_crys 0 --megnet_loss_mode True --coef_e_form 0 --num_gradient_steps "$steps"
